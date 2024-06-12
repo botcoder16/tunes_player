f11c2cbdeb21d4b7a80002d3d772afe732fc5c0d
advika10 <advikasingh.knp1@gmail.com>
2024-06-12 13:03:59 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from 'react'
-import { GlobalContext } from '../context'
+import React, { useContext, useEffect } from 'react';
+import { GlobalContext } from '../context';
 import SongItem from '../components/SongItem';
 import DiscoverSongItem from '../components/DiscoverSongItem';
+import './Home.css'; // Ensure to create or import appropriate CSS
 
 function Home() {
   const { discoverSongs, songList, loading, setDiscoverSongs } = useContext(GlobalContext);
@@ -18,26 +19,33 @@ function Home() {
     getDiscoverSongs();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div className="dot-1"></div>
+        <div className="dot-2"></div>
+        <div className="dot-3"></div>
+      </div>
+    );
+  }
 
-  if (loading) return <div>Loading... Please wait!</div>
   if (songList && songList.length > 0) {
     title = 'Results';
   } else {
-    title = 'New Releases'
+    title = 'New Releases';
   }
+
   return (
     <>
       <h2 className='font-extrbold mt-3 text-4xl truncate uppercase text-black'>{title}</h2>
-    <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
-      {
-        songList && songList.length >0 ? 
-        songList?.map(item=> <SongItem item={item}/>)
-        :  
-        discoverSongs?.map(item=> <DiscoverSongItem item={item}/>)
+      <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
+        {songList && songList.length > 0 ? 
+          songList.map(item => <SongItem key={item.id} item={item} />) : 
+          discoverSongs.map(item => <DiscoverSongItem key={item.id} item={item} />)
         }
-    </div>
-      </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
